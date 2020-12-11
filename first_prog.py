7a2e8618299f22d85f0411749c30ddda9259f6fa
KaustubhMuhuri <75669115+KaustubhMuhuri@users.noreply.github.com>
2020-12-11 15:39:23 +0530

    First python Program

---
+++ first_prog.py
@@ -0,0 +1,46 @@
+'''a,b=12,13
+if (b%2==0 or a%2==0):
+    print("Even")
+else :
+    print("Odd") '''
+
+''' x=5
+
+if x==1:
+    print("One")
+elif x==2:
+    print('Two')
+elif x==3:
+    print("Three")
+elif x==4:
+    print ("Four")
+else:
+    print("Other value") '''
+
+
+
+'''i=1
+while (i<=100):
+    if (i%3!=0 and i%5!=0):
+       print(i)
+    i=i+1'''
+
+a=int(input("Enter Side length"))
+i=1
+while i<=a:
+    j=1
+    while j<=a:
+        print("#  ",end="")
+        j=j+1
+
+    print()
+    i=i+1
+
+
+
+
+
+
+
+
+
